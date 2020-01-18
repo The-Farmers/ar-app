aecd939c4d3b182db91057498f05ad5b585cd1aa
Jeremy <tan_kai_qun97@yahoo.com.sg>
2020-01-18 14:49:05 +0800

    Add planting chicken feature

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import chicken from "./Model/chicken.gltf";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        // <!-- We must add the tap-place component to the scene so it has an effect -->
+      }
+      <a-scene
+        xrweb
+        tap-place
+        xrextras-almost-there
+        xrextras-loading
+        xrextras-runtime-error
+      >
+        {
+          // <!-- We can define assets here to be loaded when A-Frame initializes -->
+        }
+        <a-assets>
+          {
+            // <!-- Credit to Poly by Google for the model: https://poly.google.com/view/6pwiq7hSrHr -->
+          }
+          <a-asset-item id="treeModel" src={chicken}></a-asset-item>
+        </a-assets>
+
+        {
+          //<!-- The raycaster will emit mouse events on scene objects specified with the cantap class -->
+        }
+        <a-camera
+          position="0 8 0"
+          raycaster="objects: .cantap"
+          cursor="
+          fuse: false;
+          rayOrigin: mouse;"
+        ></a-camera>
+
+        <a-entity
+          light="type: directional;
+               intensity: 0.8;"
+          position="1 4.3 2.5"
+        ></a-entity>
+
+        <a-light type="ambient" intensity="1"></a-light>
+
+        {
+          //<!-- Adding the cantap class allows the ground to be clicked -->
+        }
+        <a-entity
+          id="ground"
+          class="cantap"
+          geometry="primitive: box"
+          material="color: #ffffff; transparent: true; opacity: 0.0"
+          scale="1000 2 1000"
+          position="0 -1 0"
+        ></a-entity>
+      </a-scene>
     </div>
   );
 }
