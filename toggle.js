3d70402273aab305b473137ffe88ec08413a81a1
Maxnks <maxngkaishen@gmail.com>
2020-01-19 11:53:30 +0800

    last

--- toggle.js
+++ toggle.js
@@ -12,8 +12,16 @@ function getModelName() {
       return "#tomatoModel";
     case 3:
       return "#unicornModel";
-    default:
+    case 4:
       return "#flyingSaucerModel";
+    case 5:
+      return "#pagodaModel"
+    case 6:
+      return "#lilyPadModel"
+    case 7:
+      return "#shrekModel"
+    default:
+      return "roseModel"
   }
 }
 
@@ -25,7 +33,15 @@ function getModalScale() {
       return "0.05 0.05 0.05";
     case 3:
       return "0.4 0.4 0.4";
-    default:
+    case 4:
       return "200 200 200";
+    case 5:
+      return "0.18 0.18 0.18"
+    case 6:
+      return "1 1 1"
+    case 7:
+      return "5 5 5"
+    default:
+      return "1 1 1"
   }
 }
